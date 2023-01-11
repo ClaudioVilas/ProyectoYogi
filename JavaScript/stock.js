44e7c7c9eb3157e10f261c8c7e002e7fece7fb2a
Claudio Vilas <claudiogvilas@gmail.com>
2023-01-11 10:43:39 -0300

    primer paso

---
+++ JavaScript/stock.js
@@ -0,0 +1,9 @@
+let stockProductos = [
+
+    { id: 1, cantidad: 1, nombre : "Campera clasica 1", tipo : "Campera De jean", desc: 'Campera clasica de jean con ganesha', precio: 7500, talle: 'Large', img: "./imagenes/324932736_688404062835492_449787591247790257_n.jpg"},
+    { id: 2, cantidad: 1, nombre : "Campera clasica 2", tipo : "Campera De jean", desc: 'Campera clasica de jean con ganesha', precio: 7500, talle: 'Large', img: "./imagenes/325133456_1321546618701045_4908353240447954853_n.jpg"},
+    { id: 3, cantidad: 1, nombre : "Campera clasica 3", tipo : "Campera De jean", desc: 'Campera clasica de jean con ganesha', precio: 7500, talle: 'Large', img: "./imagenes/325139568_691597282514712_3659647015475168674_n.jpg"},
+    { id: 4, cantidad: 1, nombre : "Campera clasica 4", tipo : "Campera De jean", desc: 'Campera clasica de jean con ganesha', precio: 7500, talle: 'Large', img: "./imagenes/325144198_480087400971830_6182043279986196034_n.jpg"},
+    { id: 5, cantidad: 1, nombre : "Campera clasica 5", tipo : "Campera De jean", desc: 'Campera clasica de jean con ganesha', precio: 7500, talle: 'Large', img: "./imagenes/325217870_886591785818699_1776214322468277334_n.jpg"},
+
+]
